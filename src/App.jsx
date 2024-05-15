57962f5d8909fb7daa512d7f1c85f326d43d5ce3
manuellaN <manuella.nogueira02@gmail.com>
2024-05-15 11:04:51 -0300

    projeto1505

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './App.css'
+
+const App = () => (
+ <>
+   <div id='root'>
+    <section>
+      <header className="header">
+        AULA REACT
+      </header>
+      <div id="conteudo">
+      AULA INICIAL DE REATJS
+      </div>
+      <footer className="footer">
+        RETOMADA
+      </footer>
+    </section>
+   
+   </div>
+   
+ </>
+
+)
+
+export default App;
+
+const App = () => (
+  <>
+   <div id='root'>
+    <Componente1 />
+    <Componente1 />
+    <Componente1 />
+    <Componente1 />
+   </div>
+  </>
+);
